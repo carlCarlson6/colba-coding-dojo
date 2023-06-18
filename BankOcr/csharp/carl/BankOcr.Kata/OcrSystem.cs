933077205bdb61f0951bad666e7dc76a88da2bef
carl <carlos.acitores@colba.es>
2023-06-18 19:59:36 +0200

    use case 2

--- BankOcr/csharp/carl/BankOcr.Kata/OcrSystem.cs
+++ BankOcr/csharp/carl/BankOcr.Kata/OcrSystem.cs
@@ -12,8 +12,21 @@ public static class OcrSystem
         {
             var group1 = charactersNumbers.Skip(3 * (i - 1)).Take(3);
             var group2 = charactersNumbers.Skip(3 * (i - 1) + 27).Take(3);
-            var group3 = charactersNumbers.Skip(3 * (i - 1 ) + 2*27).Take(3);
+            var group3 = charactersNumbers.Skip(3 * (i - 1) + 2 * 27).Take(3);
             yield return new Number(group1.Concat(group2).Concat(group3));
         }
-    } 
+    }
+
+    public static bool ValidateAccountNumber(int[] accountNumber) => 
+        SumNumber(accountNumber) % 11 == 0;
+
+    private static int SumNumber(IReadOnlyList<int> accountNumber)
+    {
+        var sum = 0;
+        for (var i = 0; i < 9; i++)
+        {
+            sum += (9 - i) * accountNumber[i];
+        }
+        return sum;
+    }
 }
